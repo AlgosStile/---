f20cee4bef57b5240314f54f60588d36b35c3d72
Oleg Todor <oleg.todor@mail.ru>
2022-08-29 12:43:11 +0300

    Создали папку Example002_HelloUser и написали программу для ввода поля

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// Поле ввода имени для пользователя
+Console.WriteLine(" Введите Ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+// Это код команды с начала новой строки, а можно вместо Console.WriteLine заменить на Console.Write
+//таким образом поле ввода будет с тойже строке где и поле "Введите Ваше имя"
