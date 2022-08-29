062214acaa202730d2e2270215d5f868e9ed0cf9
Oleg Todor <oleg.todor@mail.ru>
2022-08-29 14:36:14 +0300

    Добавили код для программы Приветсвия в папку Example005_if_else

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа приветсвия. User вводит свое имя и программа 
+//здоровается, в ответ фраза: Ура это же Маша.
+Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+
+
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
