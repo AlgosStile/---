2631dda54d5a66d09dabba085a8e1f62054c4e0d
Oleg Todor <oleg.todor@mail.ru>
2022-08-29 14:07:34 +0300

    Добавили Random

--- Example03_Dev/Program.cs
+++ Example03_Dev/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿// Программа/код для выполнения задачи деления НЕ целых чисел, если нежно деление целых чисел
+// то вместо double указываем int.
+double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
